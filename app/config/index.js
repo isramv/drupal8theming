7a76da00e24a656e57393a7d831444038385fdae
Israel M <isramv@gmail.com>
2018-03-23 17:55:11 -0600

    updated port binding webpack

--- app/config/index.js
+++ app/config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: 'localhost', // can be overwritten by process.env.HOST
+    host: '0.0.0.0', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
@@ -27,6 +27,7 @@ module.exports = {
     // If true, eslint errors and warnings will also be shown in the error overlay
     // in the browser.
     showEslintErrorsInOverlay: false,
+    disableHostCheck: true,
 
     /**
      * Source Maps
